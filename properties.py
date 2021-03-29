e0e96c110de6af2c09eea940c0664422fe840071
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2021-03-29 09:35:01 +0300

    Update properties.py

--- properties.py
+++ properties.py
@@ -1,4 +1,4 @@
 import json
 with open('properties.json') as f:
 	data = json.load(f)
-print(data['status'])
+print("Here you can edit your bot's properties. To not chage a propertie, leave it blank (just press ENTER)")
